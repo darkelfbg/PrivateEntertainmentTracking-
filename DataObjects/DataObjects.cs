fdd0d2b0358e9ed85e0e64480d5447e3552fc680
rivanov123 <r_ivanov123@abv.bg>
2013-12-05 13:39:46 +0200

    you auxk

---
+++ DataObjects/DataObjects.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    [DataContract]
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        public bool RememberMe { get; set; }
+
+        public bool IsValid(User user)
+        {
+            return (UserName == user.UserName) && (Password == user.Password);
+        }
+    }
+}
---
+++ DataObjects/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+     public enum UserServiceStatusCode
+     {
+         Ok = 0,
+
+         WrongPassword = 1,
+
+         WrongUserName = 2,
+
+         UserNameTaken = 3,
+
+         Unknown = 4
+
+     }
+}
